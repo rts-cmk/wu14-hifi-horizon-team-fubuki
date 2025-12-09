4454ae93e5c2b0dd95f4f751cff528bd19942fa2
Overlordka <kopary787@gmail.com>
2025-12-09 10:20:34 +0100

    feat: added routes to pages

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,10 +1,30 @@
+import AboutUs from './pages/AboutUs'
+import Blog from './pages/Blog'
+import Brands from './pages/Brands'
+import ContactUs from './pages/ContactUs'
+import Events from './pages/Events'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import './style/main.sass'
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/brands" element={<Brands />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </Router>
     </>
   )
 }
