ee118701338e5a16aa95c40228c56f7b100061a3
Overlordka <kopary787@gmail.com>
2025-12-09 09:45:05 +0100

    feat:

---
+++ project/src/pages/Brands.jsx
@@ -0,0 +1,7 @@
+export default function Brands() {
+  return (
+    <div>
+      <h1>Brands Page</h1>
+    </div>
+  )
+}
---
+++ project/src/pages/ContactUs.jsx
@@ -0,0 +1,7 @@
+export default function ContactUs() {
+  return (
+    <div>
+      <h1>ContactUs Page</h1>
+    </div>
+  )
+}
