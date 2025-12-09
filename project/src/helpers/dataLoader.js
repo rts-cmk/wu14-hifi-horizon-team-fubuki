11108d0634272c21a51278ff80a2b4830ed70b07
David Majholt <d.boy.dragoz@gmail.com>
2025-12-09 14:03:47 +0100

    feat: updated dataloader file

--- project/src/helpers/dataLoader.js
+++ project/src/helpers/dataLoader.js
@@ -1,3 +1,15 @@
 export default async function dataLoader() {
-	return ({ success: true })
+	let fetchedData = {}
+
+	try {
+		await fetch("http://localhost:3000/api/products")
+			.then(response => response.json())
+			.then(data => {
+				fetchedData = data.result
+			})
+	} catch (err) {
+		console.error(err)
+	}
+
+	return fetchedData
 }
