84f0413fd3818741ecdabe606a475e5fa6db8c68
Overlordka <kopary787@gmail.com>
2025-12-10 10:13:05 +0100

    fix: :memo: input search on header

--- project/src/components/header.jsx
+++ project/src/components/header.jsx
@@ -7,7 +7,7 @@ import Logo from "../assets/icons/logo.svg?react";
 export default function Header() {
   return (
     <header className="header">
-      <section className="header__section-left">
+      <div className="header__section-left">
         <Link to="/" className="header__link-logo">
           <Logo />
         </Link>
@@ -18,15 +18,15 @@ export default function Header() {
             <li className="header__li"><Link to="/contact">CONTACT</Link></li>
           </ul>
         </nav>
-      </section>
-      <section className="header__section-right">
-        <div className="header__div-search">
-          <input className="header__input-search" type="text" placeholder="Search product..." />
-          <FaSearch className="header__icon-search" />
-        </div>
+      </div>
+      <div className="header__section-right">
+        <form className="header__div-search" action="/">
+          <input className="header__input-search" type="text" name="search" placeholder="Search product..." required />
+          <button className="header__btn-search"><FaSearch className="header__icon-search" /></button>
+        </form>
         <Link to="/profile" className="header__link-profile"><FaUser className="header__icon-profile" /></Link>
         <Link to="/cart/orders" className="header__link-add-to-cart"><FaCartShopping className="header__icon-add-to-cart" /><div className="header__div-add-to-cart">1</div></Link>
-      </section>
+      </div>
     </header>
   )
 }
