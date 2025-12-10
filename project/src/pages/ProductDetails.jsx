2a2ee75876817cb148e0df3e37e485de8fb45b70
David Majholt <d.boy.dragoz@gmail.com>
2025-12-10 13:52:11 +0100

    feat: added product details page content after id param

--- project/src/pages/ProductDetails.jsx
+++ project/src/pages/ProductDetails.jsx
@@ -1,7 +1,135 @@
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa"
+import { GiSettingsKnobs } from "react-icons/gi"
+import { useLoaderData } from "react-router"
+import { useState } from "react"
+
 export default function ProductDetails() {
-  return (
-    <div>
-      <h1>ProductDetails Page</h1>
-    </div>
-  )
+	const [amount, setAmount] = useState(1)
+	const [color, setColor] = useState(0)
+	const [image, setImage] = useState(0)
+	const productData = useLoaderData()
+
+	function lastImage() {
+
+	}
+
+	function nextImage() {
+
+	}
+
+	function removeAmount() {
+		if (amount >= 2) {
+			setAmount(amount - 1)
+		}
+	}
+
+	function addToCart(id = productData.id) {
+
+	}
+
+	return (
+		<main className="product-content">
+			{console.log(productData)}
+			<h2 className="product-content__title">
+				PRODUCT
+			</h2>
+
+			<button className="product-content__button-compare">
+				Compare
+				<GiSettingsKnobs className="product-content__button-icon" />
+			</button>
+
+			<div className="product-content__holder">
+				<figure className="product-content__image-holder">
+					<img src={`/images/${productData.images[image]}`} alt={productData.images[image]}
+						className="product-content__image"></img>
+
+					<figcaption className="product-content__image-caption">
+						<button onClick={() => lastImage()} className="product-content__image-button">
+							<FaAngleLeft />
+						</button>
+						<button onClick={() => nextImage()} className="product-content__image-button">
+							<FaAngleRight />
+						</button>
+
+						<nav className="product-content__image-small-buttons">
+							{
+								productData.images.map((elm, idx) => {
+									return (
+										<button onClick={() => setImage(idx)} key={`small-button-${idx}`}
+											className="product-content__image-button-small"></button>
+									)
+								})
+							}
+						</nav>
+					</figcaption>
+				</figure>
+
+				<section className="product-content__details">
+					<h4 className="product-content__name">{productData.name}</h4>
+					<p className="product-content__description">{productData.description}</p>
+
+					<ol className="product-content__colors">
+						{
+							productData.fields.find(field => field.type == "color")?.list.map((colorField, idx) => {
+								return <li key={colorField} className="product-content__color">
+									<div className="product-content__color-design">
+										<div>
+
+										</div>
+									</div>
+
+									<p className="product-content__color-name">{colorField}</p>
+								</li>
+							})
+						}
+					</ol>
+
+					<p className="product-content__info">
+						<span className="product-content__info-price">
+							$ {((productData.price - ((productData.price / 100)
+								* productData.discount)) * amount).toFixed(2)}
+						</span>
+
+						<span className="product-content__info-stock">
+							{(productData.status && "In stock" || "No stock")}
+						</span>
+					</p>
+
+					<div className="product-content__buttons-holder">
+						<div className="product-content__amounts">
+							<button onClick={() => removeAmount()}
+								className="product-content__amount-button">-</button>
+							<p className="product-content__amount">{amount}</p>
+							<button onClick={() => setAmount(amount + 1)}
+								className="product-content__amount-button">+</button>
+						</div>
+
+						<button onClick={() => addToCart()}
+							className="product-content__button-order">Add to cart</button>
+					</div>
+				</section>
+			</div>
+
+			<div className="product-content__line">
+			</div>
+
+			<ol className="product-content__specs">
+				{
+					productData.information.map((detail, idx) => {
+						return <li key={detail.title}
+							className={`product-content__spec ${((idx / 2) % 1 == 0 && "dark" || "light")}`}>
+							<p>
+								{detail.title}
+							</p>
+							<p>
+								{detail.value} {detail.second}
+							</p>
+
+						</li>
+					})
+				}
+			</ol>
+		</main>
+	)
 }
