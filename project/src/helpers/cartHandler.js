0fbf2be68104a3afa1f1dc919d164193d8e89417
David Majholt <d.boy.dragoz@gmail.com>
2025-12-15 08:35:04 +0100

    feat: added cart item component and added cart page content

---
+++ project/src/helpers/cartHandler.js
@@ -0,0 +1,58 @@
+export function CheckLogin() {
+
+}
+
+export function CreateInvoice() {
+
+}
+
+export function RetrieveCartItems() {
+	if (document.cookie != "") {
+		let cartItems = document.cookie.split(";")[1].split("=")[1].split(",")
+
+		if (cartItems[0] == "null") {
+			cartItems = []
+		}
+
+		return cartItems
+	} else {
+		return []
+	}
+}
+
+export function HandleCardItems(itemID = 0, quantity = 0, price = 0) {
+	let cartItems = document.cookie.split(";")[1].split("=")[1].split(",")
+	let newCartItems = []
+
+	if (cartItems[0] == "null") {
+		cartItems = []
+	}
+
+	cartItems.forEach(item => {
+		let [id, qty, prc] = item.split("-")
+
+		if (id == itemID) {
+			newCartItems.push(`${itemID}-${quantity}-${price}`)
+		} else if (qty >= 1) {
+			newCartItems.push(`${id}-${qty}-${prc}`)
+		}
+	})
+
+	if (!newCartItems.find(item => item.split("-")[0] == itemID)) {
+		newCartItems.push(`${itemID}-${quantity}-${price}`)
+	}
+
+	newCartItems.forEach((item, index) => {
+		let [id, qty, prc] = item.split("-")
+
+		if (id == itemID && quantity <= 0) {
+			newCartItems.splice(index, 1)
+		}
+	})
+
+	if (newCartItems.length == 0) {
+		newCartItems.push("null")
+	}
+
+	document.cookie = `cart=${newCartItems.join(",")}; path=/`
+}
