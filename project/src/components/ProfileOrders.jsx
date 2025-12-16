b5d27938aadd53c220f9488a6ee2d0a7c814bb29
David Majholt <d.boy.dragoz@gmail.com>
2025-12-16 13:46:28 +0100

    refactor: changed the structure of the imports and changed spaces into tabs

--- project/src/components/ProfileOrders.jsx
+++ project/src/components/ProfileOrders.jsx
@@ -1,33 +1,36 @@
+import { useRouteLoaderData } from "react-router";
 import { IoDocumentText } from "react-icons/io5";
 
 export default function ProfileContent() {
-    return (
-        <section className="content-profile-sec__section-info">
-                    <h2 className="content-profile-sec__h2-title">YOUR RECENT ORDERS</h2>
-                    <ul className="content-profile-sec__ul">
-                        <li className="content-profile-sec__li li-orders" >
-                            <div className="content-profile-sec__li-div">
-                                <div className="content-profile-sec__li-div-info">
-                                    <p><span className="content-profile-sec__li-span">Ordernumber:</span> 123532532436</p>
-                                    <p><span className="content-profile-sec__li-span">Date:</span> 02/22/2022</p>
-                                    <p><span className="content-profile-sec__li-span">Total:</span> $ 450,00</p>
-                                    <p><span className="content-profile-sec__li-span">items:</span> 6</p>
-                                </div>
-                            </div>
-                            <button className="content-profile-sec__li-button" > <IoDocumentText /> </button>
-                        </li>
-                        <li className="content-profile-sec__li li-orders" >
-                            <div className="content-profile-sec__li-div">
-                                <div className="content-profile-sec__li-div-info">
-                                    <p><span className="content-profile-sec__li-span">Ordernumber:</span> 123532532436</p>
-                                    <p><span className="content-profile-sec__li-span">Date:</span> 02/22/2022</p>
-                                    <p><span className="content-profile-sec__li-span">Total:</span> $ 450,00</p>
-                                    <p><span className="content-profile-sec__li-span">items:</span> 6</p>
-                                </div>
-                            </div>
-                            <button className="content-profile-sec__li-button" > <IoDocumentText /> </button>
-                        </li>
-                    </ul>
-                </section>
-    )
+	const ProfileData = useRouteLoaderData("profile")
+
+	return (
+		<section className="content-profile-sec__section-info">
+			<h2 className="content-profile-sec__h2-title">YOUR RECENT ORDERS</h2>
+			<ul className="content-profile-sec__ul">
+				<li className="content-profile-sec__li li-orders" >
+					<div className="content-profile-sec__li-div">
+						<div className="content-profile-sec__li-div-info">
+							<p><span className="content-profile-sec__li-span">Ordernumber:</span> 123532532436</p>
+							<p><span className="content-profile-sec__li-span">Date:</span> 02/22/2022</p>
+							<p><span className="content-profile-sec__li-span">Total:</span> $ 450,00</p>
+							<p><span className="content-profile-sec__li-span">items:</span> 6</p>
+						</div>
+					</div>
+					<button className="content-profile-sec__li-button" > <IoDocumentText /> </button>
+				</li>
+				<li className="content-profile-sec__li li-orders" >
+					<div className="content-profile-sec__li-div">
+						<div className="content-profile-sec__li-div-info">
+							<p><span className="content-profile-sec__li-span">Ordernumber:</span> 123532532436</p>
+							<p><span className="content-profile-sec__li-span">Date:</span> 02/22/2022</p>
+							<p><span className="content-profile-sec__li-span">Total:</span> $ 450,00</p>
+							<p><span className="content-profile-sec__li-span">items:</span> 6</p>
+						</div>
+					</div>
+					<button className="content-profile-sec__li-button" > <IoDocumentText /> </button>
+				</li>
+			</ul>
+		</section>
+	)
 }
