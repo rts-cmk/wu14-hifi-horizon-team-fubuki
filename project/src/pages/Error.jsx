53f4849f1a877a2a6a4d8cfef4c92fcb03b66713
David Majholt <d.boy.dragoz@gmail.com>
2025-12-19 09:21:50 +0100

    feat: updated error page and added some styling to it

--- project/src/pages/Error.jsx
+++ project/src/pages/Error.jsx
@@ -1,5 +1,14 @@
+import { FaRegFaceSadCry } from "react-icons/fa6";
+import { Link } from "react-router"
+
 export default function Error() {
 	return (
-		<p>Error</p>
+		<main className="error-content">
+
+			<FaRegFaceSadCry className="error-content__image" />
+			<h3 className="error-content__title">404</h3>
+			<p className="error-content__text">There doesn't seem to be anything here...</p>
+			<Link className="error-content__button" to={"/"}>Back to the front page</Link>
+		</main>
 	)
 }
